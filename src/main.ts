03b453b7d8015c5788da9ec2f7ddbcc517dfc2f2
Diego Navarro <diego.nava07@gmail.com>
2022-06-13 18:14:30 -0500

    Lesson three - Lights, Spotlight.
    This PR includes:
      - Adding GUI controller for objects on a scene using the `dat.gui`
        library.
      - Adding a "Orbital controller" using the controller provied by THREE.
      - Adding the ability to have a group of boxes arranged by as a grid.
      - Implementation of a Spotlight in a scene.

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,91 @@
+import {GUI} from 'dat.gui';
 import {
   BoxGeometry,
   Camera,
   DoubleSide,
-  FogExp2,
+  Group,
   Material,
   Mesh,
   MeshBasicMaterial,
+  MeshPhongMaterial,
   PerspectiveCamera,
   PlaneGeometry,
+  PointLight,
   Renderer,
   Scene,
+  SphereGeometry,
+  SpotLight,
   Vector3,
   WebGLRenderer,
 } from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import './style.css';
 
 const scene: Scene = new Scene();
-const box: Mesh = getBox(1, 1, 1);
 const plane: Mesh = getPlane(20);
+const lightSphere = getSphere(0.05);
+const boxGrid = getBoxGrid(10);
 const camera: Camera = new PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   1,
   1000
 );
-const renderer: WebGLRenderer = new WebGLRenderer();
-
-// Addinf fog to the scene.
-scene.fog = new FogExp2(0xffffff, 0.2);
+const renderer: WebGLRenderer = new WebGLRenderer({antialias: true});
+const spotLight = getSpotLight(1);
+const gui: GUI = new GUI();
 
 // Placing objects to the scene.
-scene.add(box);
+scene.add(boxGrid);
 scene.add(plane);
-
-//Box positionnig, let's put it above the plane.
-box.position.y = (box.geometry as BoxGeometry).parameters.height / 2;
+spotLight.add(lightSphere);
+scene.add(spotLight);
 
 //Plane rotation for better looking due camera perspective.
 plane.rotation.x = Math.PI / 2;
 
+//Placing the point light.
+spotLight.position.y = 2;
+
 //Camera position.
 camera.position.x = 1;
-camera.position.y = 1;
+camera.position.y = 2;
 camera.position.z = 5;
 camera.lookAt(new Vector3(0, 0, 0));
 
+//Adding GUI controllers
+gui.add(spotLight, 'intensity', 0, 10);
+gui.add(spotLight.position, 'y', 1, 15);
+gui.add(spotLight, 'penumbra', 0, 1);
+
 // Rendering the scene.
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xffffff);
+renderer.shadowMap.enabled = true;
+renderer.setClearColor('rgb(120, 120, 120)');
 document.getElementById('app')?.appendChild(renderer.domElement);
-renderScene(renderer, scene, camera);
 
-function renderScene(renderer: Renderer, scene: Scene, camera: Camera): void {
+const controls = new OrbitControls(camera, renderer.domElement);
+
+renderScene(renderer, scene, camera, controls);
+
+/**
+ * Renders a ThreeJS scene using the requestAnimationFrame API.
+ *
+ * @param {Renderer} renderer ThreeJS renderer instance.
+ * @param {Scene} scene ThreeJS scene instance.
+ * @param {Camera} camera ThreeJS camera instance.
+ * @param {OrbitControls} controls ThreeJS OrbitControls instance.
+ */
+function renderScene(
+  renderer: Renderer,
+  scene: Scene,
+  camera: Camera,
+  controls: OrbitControls
+): void {
   renderer.render(scene, camera);
-
+  controls.update();
   requestAnimationFrame(() => {
-    renderScene(renderer, scene, camera);
+    renderScene(renderer, scene, camera, controls);
   });
 }
 
@@ -65,26 +95,105 @@ function renderScene(renderer: Renderer, scene: Scene, camera: Camera): void {
  * @param {number} width The box width.
  * @param {number} height The box height.
  * @param {number} depth The box depth.
+ * @returns {Mesh} The actual box.
  */
 function getBox(width: number, height: number, depth: number): Mesh {
   const geometry: BoxGeometry = new BoxGeometry(width, height, depth);
-  const material: Material = new MeshBasicMaterial({
-    color: 0x00ff00,
+  const material: Material = new MeshPhongMaterial({
+    color: 'rgb(120, 120, 120)',
   });
+  const mesh: Mesh = new Mesh(geometry, material);
 
-  return new Mesh(geometry, material);
+  mesh.castShadow = true;
+  return mesh;
+}
+
+/**
+ * Returns a group of boxes arranged as a squared grid.
+ *
+ * @param {number} amount The amount of column and rows for the grid.
+ * @param {number} separation The space used to separte the boxes.
+ * @returns {Group} Gruop of boxes.
+ */
+function getBoxGrid(amount = 3, separation = 1.5): Group {
+  const grid: Group = new Group();
+
+  for (let x = 0; x < amount; x++) {
+    for (let y = 0; y < amount; y++) {
+      const box = getBox(1, 1, 1);
+
+      box.position.x = x * separation;
+      box.position.y = (box.geometry as BoxGeometry).parameters.height / 2;
+      box.position.z = y * separation;
+      grid.add(box);
+    }
+  }
+
+  grid.position.x = -(separation * (amount - 1)) / 2;
+  grid.position.z = -(separation * (amount - 1)) / 2;
+
+  return grid;
 }
 
 /**
  * Returns a square plane.
  *
  * @param {number} size The plane size.
+ * @returns {Mesh} The actual plane
  */
 function getPlane(size: number): Mesh {
   const geometry: PlaneGeometry = new PlaneGeometry(size, size);
-  const material: Material = new MeshBasicMaterial({
-    color: 0xff0000,
+  const material: Material = new MeshPhongMaterial({
+    color: 'rgb(120, 120, 120)',
     side: DoubleSide,
+  });
+  const mesh: Mesh = new Mesh(geometry, material);
+
+  mesh.receiveShadow = true;
+
+  return mesh;
+}
+
+/**
+ * Returns a Point of light.
+ *
+ * @param {number} intensity The initial point of light intensity.
+ * @returns {Mesh} The actual point of light.
+ */
+function getPointLight(intensity: number): PointLight {
+  const light: PointLight = new PointLight('rgb(255, 255, 255)', intensity);
+
+  light.castShadow = true;
+
+  return light;
+}
+
+/* Returns a SpotLight.
+ *
+ * @param {number} intensity The initial point of light intensity.
+ * @returns {Mesh} The actual point of light.
+ */
+function getSpotLight(intensity: number): SpotLight {
+  const light: SpotLight = new SpotLight('rgb(255, 255, 255)', intensity);
+
+  light.castShadow = true;
+  light.shadow.bias = 0.001;
+  light.shadow.mapSize.width = 2048;
+  light.shadow.mapSize.height = 2048;
+
+  return light;
+}
+
+/**
+ * Returns a Sphere.
+ *
+ * @param {number} radius The sphere radius.
+ * @returns {Mesh} The actual sphere.
+ */
+function getSphere(radius: number): Mesh {
+  const geometry: SphereGeometry = new SphereGeometry(radius, 24, 24);
+  const material: Material = new MeshBasicMaterial({
+    color: 'rgb(255, 255, 255)',
   });
 
   return new Mesh(geometry, material);
