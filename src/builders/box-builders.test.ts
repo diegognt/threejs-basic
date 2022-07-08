e0d660fbb14a9b1b36ff4bb10a6da343f834c28b
Diego Navarro <diego.nava07@gmail.com>
2022-07-08 15:28:06 -0500

    Adding Vitest a test framework.
    This PR includes:
      - Vitest used as Test framework
      - Adding the npm `test` script to run unit testing
      - Adding the npm `coverage` script to run coverage audit
      - Adding a `test.ts` file to check that Vitest is working as expected

---
+++ src/builders/box-builders.test.ts
@@ -0,0 +1,9 @@
+import {describe, expect, it} from 'vitest';
+
+describe('Basic Box builder', () => {
+  describe('The constructor', () => {
+    it('should be true', () => {
+      expect(true).toBe(true);
+    });
+  });
+});
